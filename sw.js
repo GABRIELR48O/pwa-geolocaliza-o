00b7a02bb38489c7b386c392ad3dd99f667f4013
GABRIELR48O <BIRICHE146@GMAIL.COM>
2026-09-13 22:03:12 -0300

    Refactor cache setup in service worker

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 const CACHE_NAME = 'pwa-cache-v1';
 const urlsToCache = [
   '/',
-  '/index.html',
-  '/style.css',
-  '/app.js',
-  '/manifest.json'
+  'index.html',
+  'style.css',
+  'app.js',
+  'manifest.json'
 ];
 
 self.addEventListener('install', event => {
